12b3bbb11681bd431b78928779c28054e8c2619e
katsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2021-10-02 14:39:09 +0900

    init

---
+++ src/bin/b.rs
@@ -0,0 +1,52 @@
+use std::u32;
+
+#[allow(unused_macros)]
+macro_rules! parse_line {
+    ( $t:ty ) => (
+        {
+            let mut line = String::new();
+            ::std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            iter.next().unwrap().parse::<$t>().unwrap()
+        }
+    );
+
+    ( $( $t:ty), +) => (
+        {
+            let mut line = String::new();
+            ::std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            ( $(iter.next().unwrap().parse::<$t>().unwrap()),* )
+        }
+    );
+}
+
+#[allow(unused_macros)]
+macro_rules! read_line {
+    () => {{
+        let mut line = String::new();
+        ::std::io::stdin().read_line(&mut line).unwrap();
+        line.pop();
+        line
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! parse_vec {
+    ( $t:ty ) => {{
+        let mut line = String::new();
+        ::std::io::stdin().read_line(&mut line).unwrap();
+        let iter = line.split_whitespace();
+        iter.map(|v| v.parse::<$t>().unwrap()).collect::<Vec<_>>()
+    }};
+}
+
+fn main() {
+    let (n, d, h) = parse_line!(u32, f64, f64);
+    let mut ans = 0f64;
+    for _ in 0..n {
+        let (di, hi) = parse_line!(f64, f64);
+        ans = ans.max(hi - di * (h - hi) / (d - di));
+    }
+    println!("{}", ans);
+}
